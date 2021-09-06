b5215b8b5e970f16fea46e76f35e1db24b538bdc
ChandrakantPal <sanjay161097@gmail.com>
2021-09-07 00:34:19 +0530

    mouth arc created with d3 arc

--- lets-make-a-face/main.jsx
+++ lets-make-a-face/main.jsx
@@ -8,11 +8,21 @@ const strokeWidth = 10
 const eyeOffsetX = 90
 const eyeOffsetY = 100
 const eyeRadius = 40
- 
+const mouthWidth = 20
+const mouthRadius = 140
+const mouthArc = d3.arc()
+    .innerRadius(mouthRadius)
+    .outerRadius(mouthRadius+mouthWidth)
+    .startAngle(Math.PI/2)
+    .endAngle(Math.PI*3/2)
+
 const App = () => <svg width={width} height={height}>
-<circle r={centerY - strokeWidth/2} cx={centerX} cy={centerY} fill="yellow" stroke="black" strokeWidth={strokeWidth}/>
-<circle r={eyeRadius} cx={centerX - eyeOffsetX} cy={centerY - eyeOffsetY}/>
-<circle r={eyeRadius} cx={centerX + eyeOffsetX} cy={centerY - eyeOffsetY}/>
+<g transform={`translate(${centerX},${centerY})`}>
+<circle r={centerY - strokeWidth/2}  fill="yellow" stroke="black" strokeWidth={strokeWidth}/>
+<circle r={eyeRadius} cx={-eyeOffsetX} cy={-eyeOffsetY}/>
+<circle r={eyeRadius} cx={eyeOffsetX} cy={-eyeOffsetY}/>
+<path d={mouthArc()}/>
+</g>
 </svg>
 
 // document.querySelector('#app').innerHTML = ``
