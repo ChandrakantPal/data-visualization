cf428d875569967c18d5d1b0303fd21f8d12b2bc
ChandrakantPal <sanjay161097@gmail.com>
2021-09-07 00:17:09 +0530

    svg face in react

--- lets-make-a-face/main.jsx
+++ lets-make-a-face/main.jsx
@@ -2,11 +2,17 @@ import './style.css'
 
 const width = 960
 const height = 500
-
+const centerX = width/2
+const centerY = height/2 
+const strokeWidth = 10
+const eyeOffsetX = 90
+const eyeOffsetY = 100
+const eyeRadius = 40
+ 
 const App = () => <svg width={width} height={height}>
-<circle r="245" cx="480" cy="250" fill="yellow" stroke="black" stroke-width="10"/>
-<circle r="50" cx="350" cy="180"></circle>
-<circle r="50" cx="600" cy="180"></circle>
+<circle r={centerY - strokeWidth/2} cx={centerX} cy={centerY} fill="yellow" stroke="black" strokeWidth={strokeWidth}/>
+<circle r={eyeRadius} cx={centerX - eyeOffsetX} cy={centerY - eyeOffsetY}/>
+<circle r={eyeRadius} cx={centerX + eyeOffsetX} cy={centerY - eyeOffsetY}/>
 </svg>
 
 // document.querySelector('#app').innerHTML = ``
