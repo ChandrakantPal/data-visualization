4bd155ec3760952486c96d1cf18957e424e91e17
ChandrakantPal <sanjay161097@gmail.com>
2021-09-08 23:18:37 +0530

    background face used

--- lets-make-a-face-react-ts/src/App.tsx
+++ lets-make-a-face-react-ts/src/App.tsx
@@ -1,8 +1,39 @@
 import React from 'react'
+import { arc } from 'd3'
 import './App.css'
+import BackgroundCircle from './components/BackgroundCircle'
 
 function App() {
-  return <div className="App"></div>
+  const width = 960
+  const height = 500
+  const centerX = width / 2
+  const centerY = height / 2
+  const strokeWidth = 10
+  const eyeOffsetX = 90
+  const eyeOffsetY = 100
+  const eyeRadius = 40
+  const mouthWidth = 20
+  const mouthRadius = 140
+  const mouthArc: any = arc()
+    .innerRadius(mouthRadius)
+    .outerRadius(mouthRadius + mouthWidth)
+    .startAngle(Math.PI / 2)
+    .endAngle((Math.PI * 3) / 2)
+  return (
+    <div className="App">
+      <svg width={width} height={height}>
+        <g transform={`translate(${centerX},${centerY})`}>
+          <BackgroundCircle
+            radius={centerY - strokeWidth / 2}
+            strokeWidth={strokeWidth}
+          />
+          <circle r={eyeRadius} cx={-eyeOffsetX} cy={-eyeOffsetY} />
+          <circle r={eyeRadius} cx={eyeOffsetX} cy={-eyeOffsetY} />
+          <path d={mouthArc()} />
+        </g>
+      </svg>
+    </div>
+  )
 }
 
 export default App
