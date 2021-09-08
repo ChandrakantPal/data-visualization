7e7c79df086542920f2219697d7dd0a33012594a
ChandrakantPal <sanjay161097@gmail.com>
2021-09-08 23:34:01 +0530

    props changed

--- lets-make-a-face-react-ts/src/components/Mouth.tsx
+++ lets-make-a-face-react-ts/src/components/Mouth.tsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import { arc } from 'd3-shape'
+import React, { FC } from 'react'
 
-const Mouth = ({ mouthArc }: any) => {
+const Mouth: FC<{ mouthRadius: number; mouthWidth: number }> = ({
+  mouthRadius,
+  mouthWidth,
+}) => {
+  const mouthArc: any = arc()
+    .innerRadius(mouthRadius)
+    .outerRadius(mouthRadius + mouthWidth)
+    .startAngle(Math.PI / 2)
+    .endAngle((Math.PI * 3) / 2)
   return <path d={mouthArc()} />
 }
 
