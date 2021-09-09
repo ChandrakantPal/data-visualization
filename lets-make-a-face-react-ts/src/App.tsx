75033b41405f1d5d50a7a274bf956ed6beaff56e
ChandrakantPal <sanjay161097@gmail.com>
2021-09-09 21:40:23 +0530

    face container displayed

--- lets-make-a-face-react-ts/src/App.tsx
+++ lets-make-a-face-react-ts/src/App.tsx
@@ -2,31 +2,26 @@ import React from 'react'
 import './App.css'
 import BackgroundCircle from './components/BackgroundCircle'
 import Eye from './components/Eye'
+import FaceContainer from './components/FaceContainer'
 import Mouth from './components/Mouth'
 
 const App = () => {
   const width = 960
   const height = 500
-  const centerX = width / 2
-  const centerY = height / 2
-  const strokeWidth = 10
-  const eyeOffsetX = 90
-  const eyeOffsetY = 100
-  const eyeRadius = 40
 
   return (
     <div className="App">
-      <svg width={width} height={height}>
-        <g transform={`translate(${centerX},${centerY})`}>
-          <BackgroundCircle
-            radius={centerY - strokeWidth / 2}
-            strokeWidth={strokeWidth}
-          />
-          <Eye radius={eyeRadius} cx={-eyeOffsetX} cy={-eyeOffsetY} />
-          <Eye radius={eyeRadius} cx={eyeOffsetX} cy={-eyeOffsetY} />
-          <Mouth mouthWidth={20} mouthRadius={140} />
-        </g>
-      </svg>
+      <FaceContainer
+        centerX={width / 2}
+        centerY={height / 2}
+        height={height}
+        width={width}
+      >
+        <BackgroundCircle radius={height / 2 - 10 / 2} strokeWidth={10} />
+        <Eye radius={40} cx={-90} cy={-100} />
+        <Eye radius={40} cx={90} cy={-100} />
+        <Mouth mouthWidth={20} mouthRadius={140} />
+      </FaceContainer>
     </div>
   )
 }
