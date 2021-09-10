90c431ffe3daca04aebe69f03c771d180108e45d
ChandrakantPal <sanjay161097@gmail.com>
2021-09-11 00:03:56 +0530

    fetch text cleanup

--- loading-and-parsing-data/main.js
+++ loading-and-parsing-data/main.js
@@ -1,11 +1,10 @@
 import './style.css'
 
-const url =
+const fetchText = async (url) => {
+  const response = await fetch(url)
+  return await response.text()
+}
+const csvUrl =
   'https://gist.githubusercontent.com/curran/b236990081a24761f7000567094914e0/raw/cssNamedColors.csv'
 
-const fetchText = async () => {
-  const response = await fetch(url)
-  const text = await response.text()
-  console.log(text)
-}
-fetchText()
+fetchText(csvUrl).then((text) => console.log(text))
