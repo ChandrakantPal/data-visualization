c29a827e2776ebe3d64be15ac8cf9f539f3c26e9
ChandrakantPal <sanjay161097@gmail.com>
2021-09-13 22:53:54 +0530

    circle added

--- interaction-with-react/src/App.tsx
+++ interaction-with-react/src/App.tsx
@@ -3,41 +3,17 @@ import logo from './logo.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const width = 960
+  const height = 500
+  const centerX = width / 2
+  const centerY = height / 2
+  const circleRadius = 30
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <svg width={width} height={height}>
+        <circle r={circleRadius} cx={centerX} cy={centerY} />
+      </svg>{' '}
     </div>
   )
 }
