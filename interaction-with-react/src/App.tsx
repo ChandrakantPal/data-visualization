81b3227dcca08dea760c28f987573e07df30f48a
ChandrakantPal <sanjay161097@gmail.com>
2021-09-14 23:18:49 +0530

    handleMouseMove centerX and Y in state

--- interaction-with-react/src/App.tsx
+++ interaction-with-react/src/App.tsx
@@ -1,19 +1,23 @@
-import React, { useState } from 'react'
+import React, { MouseEvent, useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
 function App() {
-  const width = 960
-  const height = 500
-  const centerX = width / 2
-  const centerY = height / 2
+  const width = window.innerWidth
+  const height = window.innerHeight
+  const [centerX, setCenterX] = useState(width / 2)
+  const [centerY, setCenterY] = useState(height / 2)
   const circleRadius = 30
-
+  const handleMouseMove = (event: MouseEvent) => {
+    const { clientY, clientX } = event
+    setCenterX(clientX)
+    setCenterY(clientY)
+  }
   return (
     <div className="App">
-      <svg width={width} height={height}>
+      <svg width={width} height={height} onMouseMove={handleMouseMove}>
         <circle r={circleRadius} cx={centerX} cy={centerY} />
-      </svg>{' '}
+      </svg>
     </div>
   )
 }
