7e0764068ec296dfa6b0b1a708e3d9ebe798d0ce
ChandrakantPal <sanjay161097@gmail.com>
2021-09-15 23:45:01 +0530

    mousePostion state for faster response as only state needs to be changed on mouse move

--- interaction-with-react/src/App.tsx
+++ interaction-with-react/src/App.tsx
@@ -5,18 +5,18 @@ import './App.css'
 function App() {
   const width = window.innerWidth
   const height = window.innerHeight
-  const [centerX, setCenterX] = useState(width / 2)
-  const [centerY, setCenterY] = useState(height / 2)
+  const initialMousePostion = { x: width / 2, y: height / 2 }
+  const [mousePostion, setMousePostion] =
+    useState<{ x: number; y: number }>(initialMousePostion)
   const circleRadius = 30
   const handleMouseMove = (event: MouseEvent) => {
     const { clientY, clientX } = event
-    setCenterX(clientX)
-    setCenterY(clientY)
+    setMousePostion({ x: clientX, y: clientY })
   }
   return (
     <div className="App">
       <svg width={width} height={height} onMouseMove={handleMouseMove}>
-        <circle r={circleRadius} cx={centerX} cy={centerY} />
+        <circle r={circleRadius} cx={mousePostion.x} cy={mousePostion.y} />
       </svg>
     </div>
   )
