ab1ecb90263e900dde6ed3d8ff6d14f9aa96f3ad
ChandrakantPal <sanjay161097@gmail.com>
2021-09-16 21:46:29 +0530

    d3 csvParse

--- loading-and-parsing-data/main.js
+++ loading-and-parsing-data/main.js
@@ -7,4 +7,10 @@ const fetchText = async (url) => {
 const csvUrl =
   'https://gist.githubusercontent.com/curran/b236990081a24761f7000567094914e0/raw/cssNamedColors.csv'
 
-fetchText(csvUrl).then((text) => console.log(text))
+fetchText(csvUrl).then((text) => {
+  const data = d3.csvParse(text)
+  const message = `${Math.round(text.length / 1024)}  kb \n${
+    data.length
+  } rows\n${data.columns.length} columns`
+  document.getElementById('text-container').textContent = message
+})
