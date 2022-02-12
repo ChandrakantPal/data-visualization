a22d712f0c1dc66459d1c71dc65db20520f0d9df
ChandrakantPal <sanjay161097@gmail.com>
2022-02-12 15:51:36 +0530

    viz js

---
+++ vega-lite-template/viz.js
@@ -0,0 +1,10 @@
+import vl from 'vega-lite-api'
+export const viz = vl
+  .markCircle({ size: 300, opacity: 0.5 })
+  .encode(
+    vl.x().fieldQ('mpg').scale({ zero: false }),
+    vl.y().fieldQ('horsepower').scale({ zero: false }),
+    vl.color().fieldN('origin'),
+    vl.size().fieldQ('weight'),
+    vl.tooltip().fieldN('name')
+  )
