a55ef582f938e32c9075ce1163d5d09155761990
ChandrakantPal <sanjay161097@gmail.com>
2022-02-14 15:11:13 +0530

    slicing  the data

--- barchart-react-d3/src/App.tsx
+++ barchart-react-d3/src/App.tsx
@@ -3,29 +3,33 @@ import { csv } from 'd3-fetch'
 import './App.css'
 import { scaleBand, scaleLinear, max } from 'd3'
 
-const App = () => {
-  const csvUrl =
-    'https://gist.githubusercontent.com/curran/0ac4077c7fc6390f5dd33bf5c06cb5ff/raw/605c54080c7a93a417a3cea93fd52e7550e76500/UN_Population_2019.csv'
-  const width = window.innerWidth
-  const height = window.innerHeight
+const csvUrl =
+  'https://gist.githubusercontent.com/curran/0ac4077c7fc6390f5dd33bf5c06cb5ff/raw/605c54080c7a93a417a3cea93fd52e7550e76500/UN_Population_2019.csv'
 
+const width = 960
+const height = 500
+
+const App = () => {
   const [data, setData] = useState<any>(null)
 
   useEffect(() => {
     const row = (d: any) => {
-      // converting string to number
       d.Population = +d['2020']
       return d
     }
-    csv(csvUrl, row).then(setData)
+    csv(csvUrl, row).then((data) => {
+      setData(data.slice(0, 10))
+    })
   }, [])
 
   if (!data) {
     return <pre>Loading...</pre>
   }
 
+  console.log(data[0])
+
   const yScale = scaleBand()
-    .domain(data?.map((d: any) => d.Country))
+    .domain(data.map((d: any) => d.Country))
     .range([0, height])
 
   const xScale = scaleLinear()
@@ -33,18 +37,16 @@ const App = () => {
     .range([0, width])
 
   return (
-    <div className="App">
-      <svg width={width} height={height}>
-        {data?.map((d: any) => (
-          <rect
-            x={0}
-            y={yScale(d.Countary)}
-            width={xScale(d.Population)}
-            height={yScale.bandwidth()}
-          />
-        ))}
-      </svg>
-    </div>
+    <svg width={width} height={height}>
+      {data.map((d: any) => (
+        <rect
+          key={d.Country}
+          y={yScale(d.Country)}
+          width={xScale(d.Population)}
+          height={yScale.bandwidth()}
+        />
+      ))}
+    </svg>
   )
 }
 
