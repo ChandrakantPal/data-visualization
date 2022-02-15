858678ab072aecfcca84fe03414ae18ea3a55154
ChandrakantPal <sanjay161097@gmail.com>
2022-02-15 13:07:19 +0530

    spacing for lable

--- barchart-react-d3/src/App.tsx
+++ barchart-react-d3/src/App.tsx
@@ -44,7 +44,11 @@ const App = () => {
         {xScale.ticks().map((tickValue) => (
           <g key={tickValue} transform={`translate(${xScale(tickValue)},0)`}>
             <line y2={innerHeight} stroke="black" />
-            <text style={{ textAnchor: 'middle' }} y={innerHeight} dy=".71em">
+            <text
+              style={{ textAnchor: 'middle' }}
+              y={innerHeight + 3}
+              dy=".71em"
+            >
               {tickValue}
             </text>
           </g>
