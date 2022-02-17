da66b956d325943db1da54189e6a7586c5f31a9d
ChandrakantPal <sanjay161097@gmail.com>
2022-02-17 12:53:17 +0530

    margins changed

--- barchart-react-d3/src/App.tsx
+++ barchart-react-d3/src/App.tsx
@@ -10,7 +10,7 @@ const csvUrl =
 
 const width = 960
 const height = 500
-const margin = { top: 20, right: 20, bottom: 20, left: 200 }
+const margin = { top: 20, right: 30, bottom: 40, left: 220 }
 
 const App = () => {
   const data = useData(csvUrl)
