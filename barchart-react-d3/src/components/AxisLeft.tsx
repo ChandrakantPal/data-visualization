60672079e8518664419d4ffc41d79dd353cce2fb
ChandrakantPal <sanjay161097@gmail.com>
2022-02-17 12:36:50 +0530

    classname tick

--- barchart-react-d3/src/components/AxisLeft.tsx
+++ barchart-react-d3/src/components/AxisLeft.tsx
@@ -2,15 +2,16 @@ import { FC } from 'react'
 
 const AxisLeft: FC<{ yScale: any }> = ({ yScale }) =>
   yScale.domain().map((tickValue: any) => (
-    <text
-      key={tickValue}
-      style={{ textAnchor: 'end' }}
-      dy=".32em"
-      x={-3}
-      y={yScale(tickValue) + yScale.bandwidth() / 2}
-    >
-      {tickValue}
-    </text>
+    <g className="tick" key={tickValue}>
+      <text
+        style={{ textAnchor: 'end' }}
+        dy=".32em"
+        x={-3}
+        y={yScale(tickValue) + yScale.bandwidth() / 2}
+      >
+        {tickValue}
+      </text>
+    </g>
   ))
 
 export default AxisLeft
