f27314a5a972429b6ea9a512c44293e64282d9de
ChandrakantPal <sanjay161097@gmail.com>
2022-02-18 14:03:49 +0530

    grid line

--- scatter-plot-react-d3/src/components/AxisLeft.tsx
+++ scatter-plot-react-d3/src/components/AxisLeft.tsx
@@ -1,8 +1,12 @@
 import { FC } from 'react'
 
-const AxisLeft: FC<{ yScale: any }> = ({ yScale }) =>
+const AxisLeft: FC<{ yScale: any; innerWidth: number }> = ({
+  yScale,
+  innerWidth,
+}) =>
   yScale.ticks().map((tickValue: any) => (
     <g className="tick" key={tickValue}>
+      <line x2={innerWidth} />
       <text
         style={{ textAnchor: 'end' }}
         dy=".32em"
