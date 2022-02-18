44755a8308a52cafe10bb6a7552bbee7ac43edd2
ChandrakantPal <sanjay161097@gmail.com>
2022-02-18 14:00:40 +0530

    axis left

---
+++ scatter-plot-react-d3/src/components/AxisLeft.tsx
@@ -0,0 +1,17 @@
+import { FC } from 'react'
+
+const AxisLeft: FC<{ yScale: any }> = ({ yScale }) =>
+  yScale.domain().map((tickValue: any) => (
+    <g className="tick" key={tickValue}>
+      <text
+        style={{ textAnchor: 'end' }}
+        dy=".32em"
+        x={-3}
+        y={yScale(tickValue)}
+      >
+        {tickValue}
+      </text>
+    </g>
+  ))
+
+export default AxisLeft
