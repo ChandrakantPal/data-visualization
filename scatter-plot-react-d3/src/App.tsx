a6a4e06e840755b6af63cad9f30fc6e043af5b25
ChandrakantPal <sanjay161097@gmail.com>
2022-02-18 13:59:59 +0530

    csvlink x scale and yscale

---
+++ scatter-plot-react-d3/src/App.tsx
@@ -0,0 +1,71 @@
+import './App.css'
+import { scaleLinear, max, format, min, extent } from 'd3'
+import { useData } from './utils/useData'
+import AxisBottom from './components/AxisBottom'
+import AxisLeft from './components/AxisLeft'
+import Marks from './components/Marks'
+
+const csvUrl =
+  'https://gist.githubusercontent.com/curran/a08a1080b88344b0c8a7/raw/639388c2cbc2120a14dcf466e85730eb8be498bb/iris.csv'
+
+const width = 960
+const height = 500
+const margin = { top: 20, right: 30, bottom: 65, left: 220 }
+const xAxisLabelOffset = 50
+
+const App = () => {
+  const data = useData(csvUrl)
+
+  if (!data) {
+    return <pre>Loading...</pre>
+  }
+
+  const innerHeight = height - margin.top - margin.bottom
+  const innerWidth = width - margin.left - margin.right
+
+  const xValue = (d: any) => d.sepal_length
+  const yValue = (d: any) => d.sepal_width
+
+  const siFormat = format('.2s')
+  const xAxisTickFormat = (tickValue: number) =>
+    siFormat(tickValue).replace('G', 'B')
+
+  const xScale = scaleLinear()
+    .domain(extent(data, xValue))
+    .range([0, innerWidth])
+
+  const yScale = scaleLinear()
+    .domain(extent(data, yValue))
+    .range([0, innerHeight])
+
+  return (
+    <svg width={width} height={height}>
+      <g transform={`translate(${margin.left},${margin.top})`}>
+        <AxisBottom
+          xScale={xScale}
+          innerHeight={innerHeight}
+          tickFormat={xAxisTickFormat}
+        />
+        <AxisLeft yScale={yScale} />
+        <text
+          className="axis-label"
+          x={innerWidth / 2}
+          y={innerHeight + xAxisLabelOffset}
+          textAnchor="middle"
+        >
+          Population in 2020
+        </text>
+        <Marks
+          data={data}
+          xScale={xScale}
+          yScale={yScale}
+          xValue={xValue}
+          yValue={yValue}
+          tooltipFormat={xAxisTickFormat}
+        />
+      </g>
+    </svg>
+  )
+}
+
+export default App
