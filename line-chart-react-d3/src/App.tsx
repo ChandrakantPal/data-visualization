14b2d15f8e424f941a254eb05a7cea3cd921d097
ChandrakantPal <sanjay161097@gmail.com>
2022-02-19 14:01:00 +0530

    csvUrl changed

--- line-chart-react-d3/src/App.tsx
+++ line-chart-react-d3/src/App.tsx
@@ -1,44 +1,86 @@
-import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
+import { scaleLinear, max, format, min, extent } from 'd3'
+import { useData } from './utils/useData'
+import AxisBottom from './components/AxisBottom'
+import AxisLeft from './components/AxisLeft'
+import Marks from './components/Marks'
 
-function App() {
-  const [count, setCount] = useState(0)
+const csvUrl =
+  'https://gist.githubusercontent.com/curran/90240a6d88bdb1411467b21ea0769029/raw/7d4c3914cc6a29a7f5165f7d5d82b735d97bcfe4/week_temperature_sf.csv'
+
+const width = 960
+const height = 500
+const margin = { top: 20, right: 30, bottom: 65, left: 90 }
+const xAxisLabelOffset = 50
+const yAxisLabelOffset = 40
+
+const App = () => {
+  const data = useData(csvUrl)
+
+  if (!data) {
+    return <pre>Loading...</pre>
+  }
+
+  const innerHeight = height - margin.top - margin.bottom
+  const innerWidth = width - margin.left - margin.right
+
+  const xValue = (d: any) => d.sepal_length
+  const xAxisLabel = 'Sepal Length'
+
+  const yValue = (d: any) => d.sepal_width
+  const yAxisLabel = 'Sepal Width'
+
+  const siFormat = format('.2s')
+  const xAxisTickFormat = (tickValue: number) =>
+    siFormat(tickValue).replace('G', 'B')
+
+  const xScale = scaleLinear()
+    .domain(extent(data, xValue))
+    .range([0, innerWidth])
+    .nice()
+
+  const yScale = scaleLinear()
+    .domain(extent(data, yValue))
+    .range([0, innerHeight])
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <svg width={width} height={height}>
+      <g transform={`translate(${margin.left},${margin.top})`}>
+        <AxisBottom
+          xScale={xScale}
+          innerHeight={innerHeight}
+          tickFormat={xAxisTickFormat}
+          tickOffset={5}
+        />
+        <text
+          className="axis-label"
+          textAnchor="middle"
+          transform={`translate(${-yAxisLabelOffset},${
+            innerHeight / 2
+          })rotate(-90)`}
+        >
+          {yAxisLabel}
+        </text>
+        <AxisLeft yScale={yScale} innerWidth={innerWidth} tickOffset={5} />
+        <text
+          className="axis-label"
+          x={innerWidth / 2}
+          y={innerHeight + xAxisLabelOffset}
+          textAnchor="middle"
+        >
+          {xAxisLabel}
+        </text>
+        <Marks
+          data={data}
+          xScale={xScale}
+          yScale={yScale}
+          xValue={xValue}
+          yValue={yValue}
+          tooltipFormat={xAxisTickFormat}
+          circleRadius={7}
+        />
+      </g>
+    </svg>
   )
 }
 
