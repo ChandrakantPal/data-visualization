1de5cc3530ea6691324bfba593ac69e766a44973
ChandrakantPal <sanjay161097@gmail.com>
2022-02-22 12:12:13 +0530

    axis left

---
+++ interactive-scatter-plot/src/components/AxisLeft.tsx
@@ -0,0 +1,21 @@
+import { FC } from 'react'
+
+const AxisLeft: FC<{
+  yScale: any
+  innerWidth: number
+  tickOffset?: number
+}> = ({ yScale, innerWidth, tickOffset = 3 }) =>
+  yScale.ticks().map((tickValue: any) => (
+    <g
+      className="tick"
+      key={tickValue}
+      transform={`translate(0,${yScale(tickValue)})`}
+    >
+      <line x2={innerWidth} />
+      <text style={{ textAnchor: 'end' }} dy=".32em" x={-tickOffset}>
+        {tickValue}
+      </text>
+    </g>
+  ))
+
+export default AxisLeft
