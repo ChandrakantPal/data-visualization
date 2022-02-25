80720ec25ce703f399a757c191c4a0fcf795624b
ChandrakantPal <sanjay161097@gmail.com>
2022-02-25 14:37:03 +0530

    clean up

--- interactive-scatter-plot/src/components/ColorLegend.tsx
+++ interactive-scatter-plot/src/components/ColorLegend.tsx
@@ -4,13 +4,12 @@ const ColorLegend: FC<{
   colorScale: any
   tickSpacing?: number
   tickSize?: number
-}> = ({ colorScale, tickSpacing = 20, tickSize = 10 }) => {
-  return colorScale.domain().map((domainValue: string, i: number) => (
+}> = ({ colorScale, tickSpacing = 20, tickSize = 10 }) =>
+  colorScale.domain().map((domainValue: string, i: number) => (
     <g key={domainValue} transform={`translate(0,${i * tickSpacing})`}>
       <circle fill={colorScale(domainValue)} r={tickSize} />
       <text>{domainValue}</text>
     </g>
   ))
-}
 
 export default ColorLegend
