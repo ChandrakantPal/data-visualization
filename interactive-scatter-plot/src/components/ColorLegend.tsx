73f3394940c3fe9f62e46cbc3297e509de3d704e
ChandrakantPal <sanjay161097@gmail.com>
2022-02-25 14:36:03 +0530

    tickSpacing and tickSize

--- interactive-scatter-plot/src/components/ColorLegend.tsx
+++ interactive-scatter-plot/src/components/ColorLegend.tsx
@@ -1,14 +1,16 @@
 import { FC } from 'react'
 
-const ColorLegend: FC<{ colorScale: any }> = ({ colorScale }) => {
-  return colorScale.domain().map((domainValue: string) => {
-    return (
-      <g key={domainValue}>
-        <circle fill={colorScale(domainValue)} />
-        <text>{domainValue}</text>
-      </g>
-    )
-  })
+const ColorLegend: FC<{
+  colorScale: any
+  tickSpacing?: number
+  tickSize?: number
+}> = ({ colorScale, tickSpacing = 20, tickSize = 10 }) => {
+  return colorScale.domain().map((domainValue: string, i: number) => (
+    <g key={domainValue} transform={`translate(0,${i * tickSpacing})`}>
+      <circle fill={colorScale(domainValue)} r={tickSize} />
+      <text>{domainValue}</text>
+    </g>
+  ))
 }
 
 export default ColorLegend
