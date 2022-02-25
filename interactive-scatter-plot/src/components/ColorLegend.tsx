561a6a3e3c6fdc0b824ec94f2915b2842faec54f
ChandrakantPal <sanjay161097@gmail.com>
2022-02-25 14:48:13 +0530

    text offset

--- interactive-scatter-plot/src/components/ColorLegend.tsx
+++ interactive-scatter-plot/src/components/ColorLegend.tsx
@@ -4,11 +4,14 @@ const ColorLegend: FC<{
   colorScale: any
   tickSpacing?: number
   tickSize?: number
-}> = ({ colorScale, tickSpacing = 20, tickSize = 10 }) =>
+  tickTextOffset?: number
+}> = ({ colorScale, tickSpacing = 20, tickSize = 10, tickTextOffset = 20 }) =>
   colorScale.domain().map((domainValue: string, i: number) => (
     <g key={domainValue} transform={`translate(0,${i * tickSpacing})`}>
       <circle fill={colorScale(domainValue)} r={tickSize} />
-      <text dy=".32em">{domainValue}</text>
+      <text x={tickTextOffset} dy=".32em">
+        {domainValue}
+      </text>
     </g>
   ))
 
