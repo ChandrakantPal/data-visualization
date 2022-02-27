93bee33074e3b6ea9f1d4f9498cad29f35380fc1
ChandrakantPal <sanjay161097@gmail.com>
2022-02-27 14:33:42 +0530

    useWorldAtlas used

--- world-map-with-cities-react-d3/src/App.tsx
+++ world-map-with-cities-react-d3/src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
 import {} from 'd3'
-import { useData } from './utils/useData'
+import { useWorldAtlas } from './utils/useWorldAtlas'
 import Marks from './components/Marks'
 
 const jsonUrl = 'https://unpkg.com/world-atlas@2.0.2/countries-50m.json'
@@ -9,7 +9,7 @@ const width = 960
 const height = 500
 
 const App = () => {
-  const data = useData(jsonUrl)
+  const data = useWorldAtlas(jsonUrl)
 
   if (!data) {
     return <pre>Loading...</pre>
