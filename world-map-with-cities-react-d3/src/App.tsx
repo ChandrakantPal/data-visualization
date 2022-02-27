6fdb8a8c8cbd709d807fbaa4f2e24b321605ab2a
ChandrakantPal <sanjay161097@gmail.com>
2022-02-27 14:34:50 +0530

    useCities hook used

--- world-map-with-cities-react-d3/src/App.tsx
+++ world-map-with-cities-react-d3/src/App.tsx
@@ -2,14 +2,19 @@ import './App.css'
 import {} from 'd3'
 import { useWorldAtlas } from './utils/useWorldAtlas'
 import Marks from './components/Marks'
+import { useCities } from './utils/useCities'
 
 const jsonUrl = 'https://unpkg.com/world-atlas@2.0.2/countries-50m.json'
+
+const csvUrl =
+  'https://gist.githubusercontent.com/curran/13d30e855d48cdd6f22acdf0afe27286/raw/0635f14817ec634833bb904a47594cc2f5f9dbf8/worldcities_clean.csv'
 
 const width = 960
 const height = 500
 
 const App = () => {
   const data = useWorldAtlas(jsonUrl)
+  const cities = useCities(csvUrl)
 
   if (!data) {
     return <pre>Loading...</pre>
