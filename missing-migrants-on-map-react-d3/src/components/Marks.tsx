b673449ef3244bf78e2ba362a2b9ae981502a93d
ChandrakantPal <sanjay161097@gmail.com>
2022-03-03 15:47:08 +0530

    marks

---
+++ missing-migrants-on-map-react-d3/src/components/Marks.tsx
@@ -0,0 +1,30 @@
+import { geoNaturalEarth1, geoPath, geoGraticule } from 'd3'
+import { FC } from 'react'
+
+const projection = geoNaturalEarth1()
+const path = geoPath(projection)
+const graticule = geoGraticule()
+
+const Marks: FC<{
+  worldAtlas: any
+  data: any
+  sizeScale: any
+  sizeValue: any
+}> = ({ worldAtlas: { land, interiors }, data, sizeScale, sizeValue }) => {
+  return (
+    <g className="marks">
+      <path className="sphere" d={`${path({ type: 'Sphere' })}`} />
+      <path className="graticules" d={`${path(graticule())}`} />
+      {land.features.map((feature: any) => (
+        <path className="land" d={`${path(feature)}`} />
+      ))}
+      {/* <path className="interiors" d={`${path(interiors)}`} /> */}
+      {data.map((d: any) => {
+        const [x, y] = projection(d.cords)
+        return <circle cx={x} cy={y} r={sizeScale(sizeValue(d))} />
+      })}
+    </g>
+  )
+}
+
+export default Marks
