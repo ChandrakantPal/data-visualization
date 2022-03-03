0db3375e6a8249f2207ad78b7b1c16fd2ce6500d
ChandrakantPal <sanjay161097@gmail.com>
2022-03-03 15:45:49 +0530

    ploting the map

--- missing-migrants-on-map-react-d3/src/App.tsx
+++ missing-migrants-on-map-react-d3/src/App.tsx
@@ -1,44 +1,40 @@
-import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
+import { max, scaleSqrt } from 'd3'
+import { useWorldAtlas } from './utils/useWorldAtlas'
+import Marks from './components/Marks'
+import { useData } from './utils/useData'
 
-function App() {
-  const [count, setCount] = useState(0)
+const jsonUrl = 'https://unpkg.com/world-atlas@2.0.2/countries-50m.json'
+
+const csvUrl =
+  'https://gist.githubusercontent.com/curran/a9656d711a8ad31d812b8f9963ac441c/raw/c22144062566de911ba32509613c84af2a99e8e2/MissingMigrants-Global-2019-10-08T09-47-14-subset.csv'
+const width = 960
+const height = 500
+
+const App = () => {
+  const worldAtlas = useWorldAtlas(jsonUrl)
+  const data = useData(csvUrl)
+
+  if (!worldAtlas || !data) {
+    return <pre>Loading...</pre>
+  }
+
+  const sizeValue = (d: any) => d['Total Dead and Missing']
+  const maxRadius = 15
+
+  const sizeScale = scaleSqrt()
+    .domain([0, max(data, sizeValue)])
+    .range([0, maxRadius])
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <svg width={width} height={height}>
+      <Marks
+        worldAtlas={worldAtlas}
+        data={data}
+        sizeScale={sizeScale}
+        sizeValue={sizeValue}
+      />
+    </svg>
   )
 }
 
