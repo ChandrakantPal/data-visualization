25224393d0c8dbe9986e9c39ebfbd5847ca601e0
ChandrakantPal <sanjay161097@gmail.com>
2022-03-04 14:11:11 +0530

    atlas hook

---
+++ missing-migrants-multiple-views-react-d3/src/utils/useWorldAtlas.ts
@@ -0,0 +1,19 @@
+import { json } from 'd3'
+import { useEffect, useState } from 'react'
+import { feature, mesh } from 'topojson'
+
+export const useWorldAtlas = (jsonUrl: string) => {
+  const [data, setData] = useState<any>(null)
+
+  useEffect(() => {
+    json(jsonUrl).then((topology: any) => {
+      const { countries, land } = topology.objects
+      setData({
+        land: feature(topology, land),
+        interiors: mesh(topology, countries, (a, b) => a !== b),
+      })
+    })
+  }, [])
+
+  return data
+}
