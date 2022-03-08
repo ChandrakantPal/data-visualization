abb5d5fab8d1d3fe5f57515dda87c3cc748e3ce5
ChandrakantPal <sanjay161097@gmail.com>
2022-03-08 15:49:30 +0530

    circles added

--- log-scales-react-d3/src/components/Marks.tsx
+++ log-scales-react-d3/src/components/Marks.tsx
@@ -1,32 +1,23 @@
 import { FC } from 'react'
 
 const Marks: FC<{
-  binnedData: any
+  data: any
   yScale: any
   xScale: any
   tooltipFormat: any
-  innerHeight: number
-}> = ({ binnedData, xScale, yScale, tooltipFormat, innerHeight }) => (
-  <g className="marks">
-    {/* <path
-      fill="none"
-      stroke="black"
-      d={`${line()
-        .x((d) => xScale(xValue(d)))
-        .y((d) => yScale(yValue(d)))
-        .curve(curveNatural)(binnedData)}`}
-    /> */}
-    {binnedData.map((d: any) => (
-      <rect
-        x={xScale(d.x0)}
-        y={yScale(d.y)}
-        width={xScale(d.x1) - xScale(d.x0)}
-        height={innerHeight - yScale(d.y)}
-      >
-        <title>{tooltipFormat(d.y)}</title>
-      </rect>
-    ))}
-  </g>
-)
+  circleRadius: number
+  xValue: any
+  yValue: any
+}> = ({ data, xScale, yScale, tooltipFormat, circleRadius, xValue, yValue }) =>
+  data.map((d: any) => (
+    <circle
+      className="mark"
+      cx={xScale(xValue(d))}
+      cy={yScale(yValue(d))}
+      r={circleRadius}
+    >
+      <title>{tooltipFormat(xValue(d))}</title>
+    </circle>
+  ))
 
 export default Marks
