08ac78c1f87e02f29b0b5848fb70149c0fb5ceac
ChandrakantPal <sanjay161097@gmail.com>
2022-03-08 15:46:04 +0530

    app

--- log-scales-react-d3/src/App.tsx
+++ log-scales-react-d3/src/App.tsx
@@ -1,44 +1,105 @@
-import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
+import {
+  scaleLinear,
+  scaleTime,
+  timeFormat,
+  extent,
+  bin,
+  timeMonths,
+  sum,
+  max,
+} from 'd3'
+import { useData } from './utils/useData'
+import AxisBottom from './components/AxisBottom'
+import AxisLeft from './components/AxisLeft'
+import Marks from './components/Marks'
 
-function App() {
-  const [count, setCount] = useState(0)
+const csvUrl =
+  'https://gist.githubusercontent.com/curran/a9656d711a8ad31d812b8f9963ac441c/raw/267eac8b97d161c479d950ffad3ddd5ce2d1f370/MissingMigrants-Global-2019-10-08T09-47-14-subset.csv'
+const width = 960
+const height = 500
+const margin = { top: 20, right: 30, bottom: 65, left: 90 }
+const xAxisLabelOffset = 60
+const yAxisLabelOffset = 45
+
+const App = () => {
+  const data = useData(csvUrl)
+
+  if (!data) {
+    return <pre>Loading...</pre>
+  }
+
+  const xValue = (d: any) => d['Reported Date']
+  const xAxisLabel = 'Reported Date'
+
+  const yValue = (d: any) => d['Total Dead and Missing']
+  const yAxisLabel = 'Total Dead and Missing'
+
+  const innerHeight = height - margin.top - margin.bottom
+  const innerWidth = width - margin.left - margin.right
+
+  const xAxisTickFormat = timeFormat('%m/%d/%Y')
+
+  const xScale = scaleTime()
+    .domain(extent(data, xValue))
+    .range([0, innerWidth])
+    .nice()
+
+  const [start, stop] = xScale.domain()
+
+  const binnedData = bin()
+    .value(xValue)
+    .domain(xScale.domain())
+    .thresholds(timeMonths(start, stop))(data)
+    .map((array) => ({
+      y: sum(array, yValue),
+      x0: array.x0,
+      x1: array.x1,
+    }))
+
+  const yScale = scaleLinear()
+    .domain([0, max(binnedData, (d) => d.y)])
+    .range([innerHeight, 0])
+    .nice()
+
+  console.log({ binnedData })
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <svg width={width} height={height}>
+      <g transform={`translate(${margin.left},${margin.top})`}>
+        <AxisBottom
+          xScale={xScale}
+          innerHeight={innerHeight}
+          tickFormat={xAxisTickFormat}
+          tickOffset={7}
+        />
+        <text
+          className="axis-label"
+          textAnchor="middle"
+          transform={`translate(${-yAxisLabelOffset},${
+            innerHeight / 2
+          })rotate(-90)`}
+        >
+          {yAxisLabel}
+        </text>
+        <AxisLeft yScale={yScale} innerWidth={innerWidth} tickOffset={7} />
+        <text
+          className="axis-label"
+          x={innerWidth / 2}
+          y={innerHeight + xAxisLabelOffset}
+          textAnchor="middle"
+        >
+          {xAxisLabel}
+        </text>
+        <Marks
+          binnedData={binnedData}
+          xScale={xScale}
+          yScale={yScale}
+          tooltipFormat={(d) => d}
+          innerHeight={innerHeight}
+        />
+      </g>
+    </svg>
   )
 }
 
