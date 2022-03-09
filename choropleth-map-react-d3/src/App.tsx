05a500414787643e9717726f6832863010ff1625
ChandrakantPal <sanjay161097@gmail.com>
2022-03-09 14:51:07 +0530

    codes

--- choropleth-map-react-d3/src/App.tsx
+++ choropleth-map-react-d3/src/App.tsx
@@ -1,8 +1,9 @@
 import './App.css'
-import { max, scaleSqrt } from 'd3'
+import { max, scaleSequential, interpolateYlOrRd } from 'd3'
 import { useWorldAtlas } from './utils/useWorldAtlas'
 import Marks from './components/Marks'
 import { useData } from './utils/useData'
+import { useCodes } from './utils/useCodes'
 
 const jsonUrl = 'https://unpkg.com/world-atlas@2.0.2/countries-50m.json'
 
@@ -14,26 +15,41 @@ const selectedYear = '2017'
 const App = () => {
   const worldAtlas = useWorldAtlas(jsonUrl)
   const data = useData(csvUrl)
+  const codes = useCodes()
 
-  if (!worldAtlas || !data) {
+  if (!worldAtlas || !data || !codes) {
     return <pre>Loading...</pre>
   }
+  const numericCodeByAlphaCode = new Map()
+  codes.forEach((code: any) => {
+    const alpha3Code = code['alpha-3']
+    const numericCode = code['country-code']
+    numericCodeByAlphaCode.set(alpha3Code, numericCode)
+  })
 
   const filteredData = data.filter((d: any) => d.Year === selectedYear)
 
-  const colorValue = (d: any) => d.adis
+  const rowByNumericCode = new Map()
+  filteredData.forEach((d: any) => {
+    const alpha3Code = d.Code
+    const numericCode = numericCodeByAlphaCode.get(alpha3Code)
+    rowByNumericCode.set(numericCode, d)
+  })
 
-  const sizeScale = scaleSqrt()
-    .domain([0, max(data, sizeValue)])
-    .range([0, maxRadius])
+  const colorValue = (d: any) => d.aids
+
+  const colorScale = scaleSequential(interpolateYlOrRd).domain([
+    0,
+    max(filteredData, colorValue),
+  ])
 
   return (
     <svg width={width} height={height}>
       <Marks
         worldAtlas={worldAtlas}
-        data={data}
-        sizeScale={sizeScale}
-        sizeValue={sizeValue}
+        rowByNumericCode={rowByNumericCode}
+        colorScale={colorScale}
+        colorValue={colorValue}
       />
     </svg>
   )
