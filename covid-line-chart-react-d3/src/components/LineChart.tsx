d03c7dbd232004827c1075ccec4a3a0281270948
ChandrakantPal <sanjay161097@gmail.com>
2022-03-11 16:15:24 +0530

    yValue

--- covid-line-chart-react-d3/src/components/LineChart.tsx
+++ covid-line-chart-react-d3/src/components/LineChart.tsx
@@ -9,8 +9,9 @@ const LineChart: FC<{ data: any; width: number; height: number }> = ({
   const xValue = (d) => d.date
   const xScale = scaleTime().domain(extent(data, xValue)).range([0, width])
 
+  const yValue = (d) => d.deathTotal
   const yScale = scaleLinear()
-    .domain([0, max(data, (d) => d.deathTotal)])
+    .domain([0, max(data, yValue)])
     .range([height, 0])
 
   return <div>LineChart</div>
