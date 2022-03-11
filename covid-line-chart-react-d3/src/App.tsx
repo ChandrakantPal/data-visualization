86a1fe7183b859081ee2fe5758464dee8612270f
ChandrakantPal <sanjay161097@gmail.com>
2022-03-11 15:47:34 +0530

    width and height

--- covid-line-chart-react-d3/src/App.tsx
+++ covid-line-chart-react-d3/src/App.tsx
@@ -3,6 +3,8 @@ import { csv } from 'd3-fetch'
 import './App.css'
 import { DSVRowArray } from 'd3-dsv'
 
+const width = document.body.clientWidth
+const height = document.body.clientHeight
 const App = () => {
   const [data, setData] = useState<DSVRowArray<string> | null>(null)
 
