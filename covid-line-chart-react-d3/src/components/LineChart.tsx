faa75b3865bfcd18bfd416cda3d8cc2bfa0ac061
ChandrakantPal <sanjay161097@gmail.com>
2022-03-11 15:59:59 +0530

    line chart component

---
+++ covid-line-chart-react-d3/src/components/LineChart.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react'
+
+const LineChart: FC<{ data: any; width: number; height: number }> = ({
+  data,
+  width,
+  height,
+}) => {
+  return <div>LineChart</div>
+}
+
+export default LineChart
