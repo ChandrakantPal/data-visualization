39a55b531cff3b2b5447b3fa9c8870658bb0d6e0
ChandrakantPal <sanjay161097@gmail.com>
2022-03-11 16:10:12 +0530

    xScale

--- covid-line-chart-react-d3/src/components/LineChart.tsx
+++ covid-line-chart-react-d3/src/components/LineChart.tsx
@@ -1,10 +1,14 @@
-import React, { FC } from 'react'
+import { FC } from 'react'
+import { extent, max, min, scaleTime } from 'd3'
 
 const LineChart: FC<{ data: any; width: number; height: number }> = ({
   data,
   width,
   height,
 }) => {
+  const xScale = scaleTime()
+    .domain(extent(data, (d) => d.date))
+    .range([0, width])
   return <div>LineChart</div>
 }
 
