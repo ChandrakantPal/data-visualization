aa891b916c8b7dedf60dc03e4c5cdfc6e07fd56d
ChandrakantPal <sanjay161097@gmail.com>
2022-03-15 15:50:24 +0530

    axis bottom

--- covid-line-chart-react-d3/src/components/XAxis.tsx
+++ covid-line-chart-react-d3/src/components/XAxis.tsx
@@ -1,7 +1,15 @@
-import { FC } from 'react'
+import { axisBottom, select } from 'd3'
+import { FC, useEffect, useRef } from 'react'
 
 const XAxis: FC<{ xScale: any }> = ({ xScale }) => {
-  return <div>XAxis</div>
+  const ref = useRef(null)
+  useEffect(() => {
+    const xAxisG = select(ref.current)
+    const xAxis = axisBottom(xScale)
+    xAxisG.call(xAxis)
+  }, [])
+
+  return <g ref={ref} />
 }
 
 export default XAxis
