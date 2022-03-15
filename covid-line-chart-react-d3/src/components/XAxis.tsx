c40af5b88c17b9925f27c2a31e48fa53ab5c49d7
ChandrakantPal <sanjay161097@gmail.com>
2022-03-15 15:53:43 +0530

    innerHeight

--- covid-line-chart-react-d3/src/components/XAxis.tsx
+++ covid-line-chart-react-d3/src/components/XAxis.tsx
@@ -1,15 +1,18 @@
 import { axisBottom, select } from 'd3'
 import { FC, useEffect, useRef } from 'react'
 
-const XAxis: FC<{ xScale: any }> = ({ xScale }) => {
-  const ref = useRef(null)
+const XAxis: FC<{ xScale: any; innerHeight: number }> = ({
+  xScale,
+  innerHeight,
+}) => {
+  const ref = useRef()
   useEffect(() => {
     const xAxisG = select(ref.current)
     const xAxis = axisBottom(xScale)
     xAxisG.call(xAxis)
   }, [])
 
-  return <g ref={ref} />
+  return <g transform={`translate(0,${innerHeight})`} ref={ref} />
 }
 
 export default XAxis
