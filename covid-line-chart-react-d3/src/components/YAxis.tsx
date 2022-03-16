7af95f56e08ad04ea9d806c64630fe84c2a519e7
ChandrakantPal <sanjay161097@gmail.com>
2022-03-16 15:56:59 +0530

    yAxis

--- covid-line-chart-react-d3/src/components/YAxis.tsx
+++ covid-line-chart-react-d3/src/components/YAxis.tsx
@@ -1,9 +1,16 @@
-import { select } from 'd3'
-import { FC, useRef } from 'react'
+import { axisLeft, select } from 'd3'
+import { FC, useEffect, useRef } from 'react'
 
-const YAxis: FC<{ yScale: any }> = ({ yScale }) => {
+const YAxis: FC<{ yScale: any; innerWidth: number }> = ({
+  yScale,
+  innerWidth,
+}) => {
   const ref = useRef()
-  const yAxisG = select(ref.current)
+  useEffect(() => {
+    const yAxisG = select(ref.current)
+    const yAxis = axisLeft(yScale).tickSize(-innerWidth)
+  }, [])
+
   return <g ref={ref} />
 }
 
