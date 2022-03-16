a9b7bc800ebd2f9dfaf3d72c89344feae8c26ebb
ChandrakantPal <sanjay161097@gmail.com>
2022-03-16 15:54:45 +0530

    YAxisG

--- covid-line-chart-react-d3/src/components/YAxis.tsx
+++ covid-line-chart-react-d3/src/components/YAxis.tsx
@@ -1,7 +1,9 @@
+import { select } from 'd3'
 import { FC, useRef } from 'react'
 
 const YAxis: FC<{ yScale: any }> = ({ yScale }) => {
   const ref = useRef()
+  const yAxisG = select(ref.current)
   return <g ref={ref} />
 }
 
