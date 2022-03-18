62f2a13cb3bba0e7a765f3e63def0f183e9f9d50
ChandrakantPal <sanjay161097@gmail.com>
2022-03-18 21:27:52 +0530

    country name commented

--- covid-line-chart-react-d3/src/utils/useData.ts
+++ covid-line-chart-react-d3/src/utils/useData.ts
@@ -9,11 +9,15 @@ const parseDay = timeParse('%m/%d/%y')
 const transform = (rawData: any) => {
   // Filter out rows that represent provinces or state
   const countriesData = rawData.filter((d: any) => !d['Province/State'])
+  // Get timeseries data for each country
   const days = rawData.columns.slice(4)
-  return days.map((day: any) => ({
-    date: parseDay(day),
-    deathTotal: rawData.map((d: any) => +d[day]).reduce(sum, 0),
-  }))
+  return countriesData.map((d: any) => {
+    // const countryName = d['Country/Region']
+    return days.map((day: any) => ({
+      date: parseDay(day),
+      deathTotal: +d[day],
+    }))
+  })
 }
 
 export const useData = (csvUrl: string) => {
