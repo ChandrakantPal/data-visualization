51c4227b5f1f61341885c107a52ef62fd57f957b
ChandrakantPal <sanjay161097@gmail.com>
2022-03-20 15:25:38 +0530

    onMouseEnter

--- covid-line-chart-react-d3/src/components/VoronoiOverlay.tsx
+++ covid-line-chart-react-d3/src/components/VoronoiOverlay.tsx
@@ -1,28 +1,36 @@
-import { FC } from 'react'
-import { Delaunay } from 'd3'
+import { FC, useMemo } from 'react'
+import * as d3 from 'd3'
 
 const VoronoiOverlay: FC<{
   innerWidth: number
   innerHeight: number
   allData: any
-  xScale: any
-  xValue: any
-  yScale: any
-  yValue: any
-}> = ({ innerHeight, innerWidth, allData, xScale, xValue, yScale, yValue }) => {
-  const points = allData.map((d: any) => [
-    xScale(xValue(d)),
-    1 + yScale(yValue(d)),
-  ])
-  const delaunay = Delaunay.from(points)
-  const voronoi = delaunay.voronoi([0, 0, innerWidth, innerHeight])
-  return (
-    <g className="voronoi">
-      {points.map((point: any, i: number) => (
-        <path fill="none" stroke="black" d={`${voronoi.renderCell(i)}`} />
-      ))}
-    </g>
-  )
+  lineGenerator: any
+  onHover: (hoverId: any) => void
+}> = ({ innerHeight, innerWidth, allData, lineGenerator, onHover }) => {
+  return useMemo(() => {
+    console.log('memoizing')
+    const points = allData.map((d: any) => [
+      lineGenerator.x()(d),
+      lineGenerator.y()(d),
+    ])
+    const delaunay = d3.Delaunay.from(points)
+    const voronoi = delaunay.voronoi([0, 0, innerWidth, innerHeight])
+    console.log({ points, delaunay, voronoi })
+
+    return (
+      <g className="voronoi">
+        {points.map((point: any, i: number) => (
+          <path
+            onMouseEnter={() => onHover(allData[i])}
+            fill="none"
+            stroke="pink"
+            d={voronoi.renderCell(i)}
+          />
+        ))}
+      </g>
+    )
+  }, [allData, lineGenerator, innerWidth, innerHeight, onHover])
 }
 
 export default VoronoiOverlay
