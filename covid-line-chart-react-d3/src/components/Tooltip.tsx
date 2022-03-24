d2750213a6ef753c1a72f1a50f12bc72d203313c
ChandrakantPal <sanjay161097@gmail.com>
2022-03-24 15:51:31 +0530

    tooltip component

---
+++ covid-line-chart-react-d3/src/components/Tooltip.tsx
@@ -0,0 +1,20 @@
+import { format, timeFormat } from 'd3'
+import { FC } from 'react'
+import { CovidData } from '../utils/types'
+
+const formatDate = timeFormat('%b %d, %Y')
+const formatComa = format(',')
+
+const Tooltip: FC<{ className: string; activeRow: CovidData }> = ({
+  className,
+  activeRow,
+}) => {
+  return (
+    <text className={className} x={-10} y={-10} textAnchor="end">
+      {activeRow.countryName}: {formatComa(activeRow.deathTotal)} deaths as of{' '}
+      {formatDate(activeRow.date)}
+    </text>
+  )
+}
+
+export default Tooltip
