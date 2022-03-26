a3a52ec5612648e03b23c77f83528be9cadba8f7
ChandrakantPal <sanjay161097@gmail.com>
2022-03-26 20:40:23 +0530

    links

--- graph-diagram-react-d3/src/App.tsx
+++ graph-diagram-react-d3/src/App.tsx
@@ -1,9 +1,16 @@
 import './App.css'
-import { forceSimulation } from 'd3'
+import { forceCenter, forceLink, forceManyBody, forceSimulation } from 'd3'
 
 function App() {
   const nodes = [{ id: 'Alice' }, { id: 'Bob' }, { id: 'Carol' }]
+  const links = [
+    { source: 0, target: 1 },
+    { source: 1, target: 2 },
+  ]
   const simulation = forceSimulation(nodes)
+    .force('charge', forceManyBody())
+    .force('link', forceLink(links))
+    .force('center', forceCenter())
   return <div className="App"></div>
 }
 
