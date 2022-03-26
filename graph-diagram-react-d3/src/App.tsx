91c592517b961f13bcfcccb8acc4de1f1109b884
ChandrakantPal <sanjay161097@gmail.com>
2022-03-26 21:19:02 +0530

    nodes moved to data

--- graph-diagram-react-d3/src/App.tsx
+++ graph-diagram-react-d3/src/App.tsx
@@ -7,14 +7,11 @@ import {
   select,
 } from 'd3'
 import { useEffect, useRef } from 'react'
+import { links, nodes } from './utils/data'
 
 function App() {
   const ref = useRef()
-  const nodes = [{ id: 'Alice' }, { id: 'Bob' }, { id: 'Carol' }]
-  const links = [
-    { source: 0, target: 1 },
-    { source: 1, target: 2 },
-  ]
+
   const simulation = forceSimulation(nodes)
     .force('charge', forceManyBody())
     .force('link', forceLink(links))
@@ -22,7 +19,14 @@ function App() {
 
   useEffect(() => {
     const svgContainer = select(ref.current)
+    const width = +svgContainer.attr('width')
+    const height = +svgContainer.attr('height')
+    const centerX = width / 2
+    const centerY = height / 2
     svgContainer.selectAll('circle').data(nodes).enter().append('circle')
+    simulation.on('tick', () => {
+      console.log('ticks')
+    })
   }, [nodes])
 
   return <svg width={960} height={500} ref={ref}></svg>
