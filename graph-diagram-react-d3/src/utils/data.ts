35c9ff8ce903b517e6f5e4f3462880d8594e7015
ChandrakantPal <sanjay161097@gmail.com>
2022-03-26 21:19:14 +0530

    data file

---
+++ graph-diagram-react-d3/src/utils/data.ts
@@ -0,0 +1,9 @@
+export const nodes = [
+  { id: 'Arts Web' },
+  { id: 'Community Vision' },
+  { id: 'Silicon Valley Creates' },
+]
+export const links = [
+  { source: 0, target: 1 },
+  { source: 0, target: 2 },
+]
