7c1552c060b6e7fe96e323cd9f158fdbc959b406
ChandrakantPal <sanjay161097@gmail.com>
2022-03-27 22:07:28 +0530

    size for nodes

--- graph-diagram-react-d3/src/utils/data.ts
+++ graph-diagram-react-d3/src/utils/data.ts
@@ -1,7 +1,7 @@
 export const nodes = [
   { id: 'Arts Web', size: 50 },
-  { id: 'Community Vision' },
-  { id: 'Silicon Valley Creates' },
+  { id: 'Community Vision', size: 10 },
+  { id: 'Silicon Valley Creates', size: 10 },
 ]
 export const links = [
   { source: 0, target: 1 },
