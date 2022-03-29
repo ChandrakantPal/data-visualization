0db350f73094142c78cc15eeb769f9c45f8c8239
ChandrakantPal <sanjay161097@gmail.com>
2022-03-29 20:42:19 +0530

    colors

---
+++ graph-diagram-react-d3/src/utils/colors.ts
@@ -0,0 +1,7 @@
+// Generated with https://paletton.com/#uid=75x0u0kigkU8ZuBdTpdmbh6rjc7
+export const colors = [
+  ['#9D4452', '#E6A6B0', '#BE6B78', '#812836', '#5B0D1A'],
+  ['#A76C48', '#F4CAAF', '#C99372', '#884E2A', '#602E0E'],
+  ['#2E6B5E', '#719D93', '#498175', '#1B584A', '#093E32'],
+  ['#538E3D', '#A6D096', '#75AC61', '#3A7424', '#1F520C'],
+]
