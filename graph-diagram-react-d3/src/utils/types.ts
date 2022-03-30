85d95051de8a238ab3b0f1495f598dd85f35bf1e
ChandrakantPal <sanjay161097@gmail.com>
2022-03-30 23:41:15 +0530

    type made condtional

--- graph-diagram-react-d3/src/utils/types.ts
+++ graph-diagram-react-d3/src/utils/types.ts
@@ -1,16 +1,16 @@
 export interface Node {
-  id: string
-  index: number
-  size: number
-  vx: number
-  vy: number
-  x: number
-  y: number
+  id?: string
+  index?: number
+  size?: number
+  vx?: number
+  vy?: number
+  x?: number
+  y?: number
 }
 
 export interface Link {
-  distance: number
-  index: number
-  source: Node
-  target: Node
+  distance?: number
+  index?: number
+  source?: Node
+  target?: Node
 }
