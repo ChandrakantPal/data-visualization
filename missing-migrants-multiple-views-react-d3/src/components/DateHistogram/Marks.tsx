c69f808d0ef6485da921841ac525bb42796a9277
ChandrakantPal <sanjay161097@gmail.com>
2022-04-08 23:44:15 +0530

    binned data  type used

--- missing-migrants-multiple-views-react-d3/src/components/DateHistogram/Marks.tsx
+++ missing-migrants-multiple-views-react-d3/src/components/DateHistogram/Marks.tsx
@@ -1,23 +1,29 @@
 import { FC } from 'react'
+import { BinnedDataType } from '../../utils/types'
 
 const Marks: FC<{
-  binnedData: any
+  binnedData: BinnedDataType[]
   yScale: any
   xScale: any
   tooltipFormat: any
   innerHeight: number
 }> = ({ binnedData, xScale, yScale, tooltipFormat, innerHeight }) => {
-  return binnedData.map((d: any) => (
-    <rect
-      className="mark"
-      x={xScale(d.x0)}
-      y={yScale(d.y)}
-      width={xScale(d.x1) - xScale(d.x0)}
-      height={innerHeight - yScale(d.y)}
-    >
-      <title>{tooltipFormat(d.y)}</title>
-    </rect>
-  ))
+  return (
+    <>
+      {binnedData.map((d, i) => (
+        <rect
+          key={`${d.x0}${i}`}
+          className="mark"
+          x={xScale(d.x0)}
+          y={yScale(d.y)}
+          width={xScale(d.x1) - xScale(d.x0)}
+          height={innerHeight - yScale(d.y)}
+        >
+          <title>{tooltipFormat(d.y)}</title>
+        </rect>
+      ))}
+    </>
+  )
 }
 
 export default Marks
