98fe45980c002c37e8aa04d0a169cb7022c007cf
ChandrakantPal <sanjay161097@gmail.com>
2022-04-11 13:01:24 +0530

    height

--- world-map-with-cities-react-d3/src/App.tsx
+++ world-map-with-cities-react-d3/src/App.tsx
@@ -10,7 +10,7 @@ const csvUrl =
   'https://gist.githubusercontent.com/curran/13d30e855d48cdd6f22acdf0afe27286/raw/0635f14817ec634833bb904a47594cc2f5f9dbf8/worldcities_clean.csv'
 
 const width = window.innerWidth
-const height = 500
+const height = window.innerHeight
 
 const App = () => {
   const worldAtlas = useWorldAtlas(jsonUrl)
