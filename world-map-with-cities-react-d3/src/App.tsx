fd6bf9a8b51b00f9090deea51a1f1a0c11f8b977
ChandrakantPal <sanjay161097@gmail.com>
2022-04-11 13:01:07 +0530

    width

--- world-map-with-cities-react-d3/src/App.tsx
+++ world-map-with-cities-react-d3/src/App.tsx
@@ -9,7 +9,7 @@ const jsonUrl = 'https://unpkg.com/world-atlas@2.0.2/countries-50m.json'
 const csvUrl =
   'https://gist.githubusercontent.com/curran/13d30e855d48cdd6f22acdf0afe27286/raw/0635f14817ec634833bb904a47594cc2f5f9dbf8/worldcities_clean.csv'
 
-const width = 960
+const width = window.innerWidth
 const height = 500
 
 const App = () => {
@@ -26,6 +26,8 @@ const App = () => {
   const sizeScale = scaleSqrt()
     .domain([0, max(cities, sizeValue)])
     .range([0, maxRadius])
+
+  console.log({ cities })
 
   return (
     <svg width={width} height={height}>
